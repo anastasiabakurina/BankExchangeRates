8410b00b0c4f0a343d028a5bcdc36969954a33db
Nastassiya <anastassia.kozhan@gmail.com>
2020-01-22 16:51:58 +0300

    added telegrambot

--- src/main/java/bankExchangeRates/main/Main.java
+++ src/main/java/bankExchangeRates/main/Main.java
@@ -1,12 +1,27 @@
 package bankExchangeRates.main;
 
 import bankExchangeRates.service.Service;
+import bankExchangeRates.service.TelegramBot;
+import org.telegram.telegrambots.ApiContext;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 public class Main{
     public static void main(String[] args) {
-        Service service = new Service();
-        service.startParse();
-        service.checkDBTable();
-        System.out.println("---" + service.getAddressMinCurrency());
+        try {
+            ApiContextInitializer.init();
+            TelegramBotsApi telegram = new TelegramBotsApi();
+            DefaultBotOptions options = ApiContext.getInstance(DefaultBotOptions.class);
+            telegram.registerBot(new TelegramBot(options));
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+
+//        Service service = new Service();
+//        service.startParse();
+//        service.checkDBTable();
+//        System.out.println("---" + service.getAddressMinCurrency());
     }
 }
